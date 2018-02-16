fccf01c2a2bc5d2b55ea99d7579a0b5417503f95
Puneet Soni <35918309+PuneetSoni4@users.noreply.github.com>
2018-02-16 18:04:39 +0530

    Added class "SUMPOWERSET"
    Now added sum_powerset method in a class. Support for test-cases.

--- sum_powerset_using_combination.py
+++ sum_powerset_using_combination.py
@@ -1,29 +1,33 @@
 from itertools import combinations
 
-def sum_powerset(data):
-    """ To generate and return powerset elements of given data """
+class SUMPOWERSET(object):
+    """ Sum powerset class """
 
-    try:
-        # params
-        result_list = []
+    def __init__(self):
+        """ init method """
+        self.result_list = []
 
-        # converting data to list
-        data_list = list(data)
+    def sum_powerset(self, data):
+        """ To generate and return sum of powerset elements of given data """
 
-        # getting all possible combinations for given data list items
-        for size in range(len(data_list) + 1):
-            data_combinations = combinations(data_list, size)
-            for element in data_combinations:
-                # extending result list to merge elements of data_combinations
-                result_list.extend(list(element))
-
-        # converting/mapping every element of result_list to integer
         try:
-            result_elements = list(map(int, result_list))
-        except ValueError:
-            return -1
+            # converting data to list
+            data_list = list(data)
 
-        # returning sum of list elements
-        return sum(result_elements)
-    except Exception as exception:
-        return "Caught exception. {0}".format(str(exception))
+            # getting all possible combinations for given data list items
+            for size in range(len(data_list) + 1):
+                data_combinations = combinations(data_list, size)
+                for element in data_combinations:
+                    # extending result list to merge elements of data_combinations
+                    self.result_list.extend(list(element))
+
+            # converting/mapping every element of result_list to integer
+            try:
+                result_elements = list(map(int, self.result_list))
+            except ValueError:
+                return -1
+
+            # returning sum of list elements
+            return sum(result_elements)
+        except Exception as exception:
+            return "Caught exception. {0}".format(str(exception))
